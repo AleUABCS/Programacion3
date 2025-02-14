e47cba0c61434b810ddb83bc519e90fd33916244
alex2 <alex2@PC-Ale>
2025-02-13 19:34:34 -0700

    Unidad 2 Ejercicio 7

--- Ventanas/src/Ventana.java
+++ Ventanas/src/Ventana.java
@@ -1,31 +1,40 @@
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame {
 	
 	public Ventana () {
-		setLayout(null);
 		setTitle("Título de la ventana");
 		setVisible(true);
 		setSize(500,400);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		//Panel
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.decode("#b0c2f2"));
+		panel.setSize(getSize());
+		add(panel);
+		panel.setLayout(null);
+		
 		//Etiqueta
 		JLabel etiqueta = new JLabel();
 		etiqueta.setText("Holaaa");
-		etiqueta.setBounds(230, 100, 50, 50);
-		add(etiqueta);
+		panel.add(etiqueta);
+		etiqueta.setBounds(200, 100, 150, 50);
+		etiqueta.setFont(new Font("Consolas", Font.BOLD, 30));
 		
 		//Botón
 		JButton boton = new JButton();
 		boton.setText("Botón");
+		panel.add(boton); 
 		boton.setBounds(200, 150, 100, 30);
-		add(boton);
 	}
 	
 }
